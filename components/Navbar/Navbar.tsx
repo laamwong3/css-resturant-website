eaeac797f31a89c65990627557b52b5c74963afd
Laam Wong <laamwong@Laams-iMac.local>
2022-10-23 03:00:49 +0800

    add components

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,7 +1,11 @@
 import React from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdOutlineRestaurantMenu } from "react-icons/md";
+import styles from "./Navbar.module.css";
+import { images } from "../../constants";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return <div className={`${styles.text2} ${styles.text3} text`}>Navbar</div>;
 };
 
 export default Navbar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,15 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import AboutUs from "../components/AboutUs/AboutUs";
+import Chef from "../components/Chef/Chef";
+import FindUs from "../components/FindUs/FindUs";
+import Footer from "../components/Footer/Footer";
+import Gallery from "../components/Gallery/Gallery";
+import Header from "../components/Header/Header";
+import Introduction from "../components/Introduction/Introduction";
+import Laurels from "../components/Laurels/Laurels";
+import Menu from "../components/Menu/Menu";
+import Navbar from "../components/Navbar/Navbar";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
@@ -11,6 +20,16 @@ const Home: NextPage = () => {
         <title>Restuarant</title>
         <meta name="description" content="resturant website" />
       </Head>
+      <Navbar />
+      <Header />
+      <AboutUs />
+      <Menu />
+      <Chef />
+      <Introduction />
+      <Laurels />
+      <Gallery />
+      <FindUs />
+      <Footer />
     </>
   );
 };
