056683dc2fc1e4fbb9a4a19e36c2e9a2c6daece2
Laam Wong <laamwong@Laams-iMac.local>
2022-10-22 17:23:01 +0800

    create folder structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import AboutUs from "../components/AboutUs/AboutUs";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
@@ -10,7 +11,6 @@ const Home: NextPage = () => {
         <title>Restuarant</title>
         <meta name="description" content="resturant website" />
       </Head>
-      <div>HELLO</div>
     </>
   );
 };
