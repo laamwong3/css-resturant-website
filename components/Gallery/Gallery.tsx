d1c4b7e11ccdc5197d70435d4abc7c13ab2599ce
Laam Wong <laamwong3@gmail.com>
2022-11-07 13:14:42 +0800

    add laurels

--- components/Gallery/Gallery.tsx
+++ components/Gallery/Gallery.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import SubHeading from "../SubHeading/SubHeading";
+import { images } from "../../constants";
+import {
+  BsInstagram,
+  BsArrowLeftShort,
+  BsArrowRightShort,
+} from "react-icons/bs";
+import styles from "./Gallery.module.css";
 
 const Gallery = () => {
-  return <div>Gallery</div>;
+  return (
+    <div className={`${styles.gallery} flex__center`}>
+      <div className={styles.gallery_content}>
+        <SubHeading title="Instagram" />
+      </div>
+    </div>
+  );
 };
 
 export default Gallery;
--- components/Laurels/Laurels.tsx
+++ components/Laurels/Laurels.tsx
@@ -1,16 +1,48 @@
-import React from "react";
+import React, { FC } from "react";
 import SubHeading from "../SubHeading/SubHeading";
-import { images } from "../../constants";
+import { images, data } from "../../constants";
 import styles from "./Laurels.module.css";
+import Image, { StaticImageData } from "next/image";
+
+interface AwardCardProps {
+  award: {
+    imgUrl: StaticImageData;
+    title: string;
+    subtitle: string;
+  };
+}
+
+const AwardCard: FC<AwardCardProps> = ({
+  award: { imgUrl, subtitle, title },
+}) => (
+  <div className={styles.award_card}>
+    <div className={styles.award_card_img}>
+      <Image src={imgUrl} alt="awards" />
+    </div>
+    <div className={styles.award_card_content}>
+      <p className="p__cormorant" style={{ color: "#DCCA87" }}>
+        {title}
+      </p>
+      <p className="p__cormorant">{subtitle}</p>
+    </div>
+  </div>
+);
 
 const Laurels = () => {
   return (
     <div className="app__bg app__wrapper section__padding" id="awards">
-      <div className={styles.info}>
+      <div className="app__wrapper_info">
         <SubHeading title="Awards $ recognition" />
         <h1 className="headtext__cormorant">Our Laurels</h1>
+        <div className={styles.info_awards}>
+          {data.awards.map((award, index) => (
+            <AwardCard key={index} award={award} />
+          ))}
+        </div>
       </div>
-      <div className={styles.img}></div>
+      <div className={styles.img}>
+        <Image src={images.laurels} alt="laurels" />
+      </div>
     </div>
   );
 };
